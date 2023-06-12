8ffdaa52bec89cfe6953e6466d24acaafa223d20
roniy68 <ahmed.11900449@lpu.in>
2023-06-12 13:53:17 +0530

    🔥 Add Structure to the React Component

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default () => (
     <>
+        <Navbar />
         <div> Hello From React</div>
+        <Footer />
     </>
 );
