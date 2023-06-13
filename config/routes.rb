2508a02b72a429bc5aa7cf08e1e2b834a397dae0
roniy68 <ahmed.11900449@lpu.in>
2023-06-14 02:27:40 +0530

    🔥 Edit The Routes in cars api
    ✅ create, show, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'cars/index'
-      get 'cars/create'
-      get 'cars/show'
-      get 'cars/destroy'
+      post 'cars/create'
+      get '/show/:id', to: 'cars#show'
+      delete '/destroy/:id', to: 'cars#destroy'
     end
   end
   root 'pages#index'
+  get '/*path' => 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
   # Cathes All Route
-  get '*path', to: 'pages#index', via: :all
+  # get '*path', to: 'pages#index', via: :all
 end
