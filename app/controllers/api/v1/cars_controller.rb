c66bd3f1a2fe2c508e9dc47e36333e57793f5895
roniy68 <ahmed.11900449@lpu.in>
2023-06-14 02:41:51 +0530

    🔥 Add the other two action to the cars controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,15 +1,36 @@
 class Api::V1::CarsController < ApplicationController
+  before_action :set_car, only: %i[show destroy]
+
   def index
     cars = Car.all.order(created_at: :desc)
     render json: cars
   end
 
   def create
+    car = Car.create!(car_params)
+    if car
+      render json: car      
+    else
+      render json: car.errors      
+    end
   end
 
   def show
+    render json: @car
   end
 
   def destroy
+    @car&.destroy
+    render json: { message: 'Car Deleted Successfully'}
+  end
+
+  private 
+
+  def car_params
+    params.permit(:name, :model, :price, :description, :photo)
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
   end
 end
