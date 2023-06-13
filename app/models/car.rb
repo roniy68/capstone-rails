139f9917e3ca5c92e76be8b599b3ee98f1ffbb0e
roniy68 <ahmed.11900449@lpu.in>
2023-06-14 02:14:59 +0530

    🔥 Define a generic Car Model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
 end
