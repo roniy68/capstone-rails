9d222f3604e1b791ba82f609acb52144e288f43e
Salwa99 <s.ritage99@gmail.com>
2023-06-13 22:12:26 +0100

    Add route for cars

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import ReservationForm from "../components/fragments/navigation/ReservationForm"
 import AddCar from "../components/fragments/navigation/AddCar";
+import Cars from "../components/fragments/navigation/Cars";
 
 export default (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/reserve" component={ReservationForm} />
-            <Route path="/addcar" component={AddCar} />
-            {/* More Routes here */}
-        </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reserve" component={ReservationForm} />
+      <Route path="/addcar" component={AddCar} />
+      <Route path="/cars" component={Cars} />
+      {/* More Routes here */}
+    </Routes>
+  </Router>
 );
