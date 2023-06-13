352b90e80b945a8eed34c05e46ad4ca2be33d0be
roniy68 <ahmed.11900449@lpu.in>
2023-06-14 02:18:03 +0530

    🔥 Define User Model and Relationship with    CAR and Reservation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :cars, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 3 }
 end
