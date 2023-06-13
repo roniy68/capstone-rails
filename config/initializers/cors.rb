b4a5c01b9d7cba225749c9ab1eeedee439ce5fa1
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-13 23:16:05 +0500

    Remove rubocop errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,10 +16,10 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins '*'
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
