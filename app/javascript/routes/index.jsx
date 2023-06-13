718e338740e4f6f75606c5b4fb639a99c037ed70
Salwa99 <s.ritage99@gmail.com>
2023-06-13 22:20:30 +0100

    Add route for my reservations

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -4,6 +4,8 @@ import Home from "../components/Home";
 import ReservationForm from "../components/fragments/navigation/ReservationForm"
 import AddCar from "../components/fragments/navigation/AddCar";
 import Cars from "../components/fragments/navigation/Cars";
+import MyReservations from "../components/fragments/navigation/MyReservations";
+
 
 export default (
   <Router>
@@ -12,6 +14,7 @@ export default (
       <Route path="/reserve" component={ReservationForm} />
       <Route path="/addcar" component={AddCar} />
       <Route path="/cars" component={Cars} />
+      <Route path="/myreservations" component={MyReservations} />
       {/* More Routes here */}
     </Routes>
   </Router>
