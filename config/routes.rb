f467afbbd61aeaffe92ff011958975315d559a0e
Ahmed Hasan Rony <ahmed.11900449@lpu.in>
2023-06-16 03:43:46 +0530

    Update routes.rb
    Test If that works

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'cars/index'
-      post 'cars/create'
+      post 'cars/create' to: 'cars#create'
       get '/show/:id', to: 'cars#show'
       delete '/destroy/:id', to: 'cars#destroy'
     end
