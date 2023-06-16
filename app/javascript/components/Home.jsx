88be8b214926fc6900cb9321ff47c36840c054f3
Salwa99 <s.ritage99@gmail.com>
2023-06-16 17:15:22 +0100

    Wrap all the styling in a single folder

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import Footer from "./fragments/Footer";
 import Main from "./fragments/Main";
 import Navigation from "./fragments/navigation/Navigation";
-import MyReservations from "./fragments/navigation/MyReservations";
 
 export default () => (
   <>
     {/* Navbar goes here */}
-    <MyReservations />
+    <Navigation />
 
     {/* Main Content here */}
     <Main />
