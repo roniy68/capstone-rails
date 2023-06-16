e3748d776a8388395fb007f7f5cfb16b80fc294e
roniy68 <ahmed.11900449@lpu.in>
2023-06-16 13:39:07 +0530

    🔥 Working Copy Of API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,4 @@
 class Api::V1::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token
+  # Some more Api Config
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,15 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'cars', to: 'cars#index'
-      post 'cars/create', to: 'cars#create'
+      post 'car/create', to: 'cars#create'
       get '/car/:id', to: 'cars#show'
       delete '/destroy/:id', to: 'cars#destroy'
     end
   end
+  
   root 'pages#index'
-  get '/*path' => 'pages#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # Cathes All Route
-  # get '*path', to: 'pages#index', via: :all
+  # Cathes All othr Routes
+  get '*path', to: 'pages#index', via: :all
+  # get '/*path' => 'pages#index'
 end
