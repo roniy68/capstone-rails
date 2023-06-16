f9bab6f2afac8b33bdb5673a8664c94f078914ba
Salwa99 <s.ritage99@gmail.com>
2023-06-16 11:40:31 +0100

    Create the reservation table

--- db/migrate/20230613202858_create_reservations.rb
+++ db/migrate/20230613202858_create_reservations.rb
@@ -1,9 +1,11 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      t.string :city
-      t.date :pick_up
-      t.date :return_date
+      t.string :car_name
+      t.string :car_model
+      t.date :start_date
+      t.date :end_date
+
       t.references :user, null: false, foreign_key: true
       t.references :car, null: false, foreign_key: true
 
