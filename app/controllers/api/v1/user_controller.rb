38c359459b9b83a13bb127a05a9647df4f457350
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-16 18:26:13 +0500

    Add user controller

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.find_or_initialize_by(username: params[:username])
+    if user.save
+      render json: { message: 'Username saved successfully' }
+    else
+      render json: { error: 'Failed to save username' }, status: :unprocessable_entity
+    end
+  end
+end
