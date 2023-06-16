091b21cee059552bd7ad44313117f8f0b611d688
Salwa99 <s.ritage99@gmail.com>
2023-06-16 11:44:39 +0100

    Define reservation routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
       post 'car/create', to: 'cars#create'
       get '/car/:id', to: 'cars#show'
       delete '/car/destroy/:id', to: 'cars#destroy'
+
+      get 'reservations', to: 'reservations#index'
+      post 'reservation/create', to: 'reservations#create'
+      get '/reservation/:id', to: 'reservations#show'
+      delete '/reservation/destroy/:id', to: 'reservations#destroy'
     end
   end
 
