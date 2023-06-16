de24857edb845eb96ae82806034a22ab2cc34425
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-16 18:35:38 +0500

    Update name of controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+  
   def create
     user = User.find_or_initialize_by(username: params[:username])
     if user.save
