0455013562185d71a3d097ec93af5e1016e0243e
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-16 18:27:25 +0500

    Update routes

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -5,16 +5,20 @@ import ReservationForm from "../components/fragments/navigation/ReservationForm"
 import AddCar from "../components/fragments/navigation/AddCar";
 import Cars from "../components/fragments/navigation/Cars";
 import MyReservations from "../components/fragments/navigation/MyReservations";
+import Login from "../components/fragments/login/login";
+import Body from "../components/fragments/Initialpage/initial";
 
 
 export default (
   <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Body />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/reserve" component={ReservationForm} />
       <Route path="/addcar" component={AddCar} />
       <Route path="/cars" component={Cars} />
       <Route path="/myreservations" component={MyReservations} />
+      <Route path="/login" element={<Login />} />
       {/* More Routes here */}
     </Routes>
   </Router>
