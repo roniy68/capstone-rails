dc71cc0d7473b3abbee3225dad52e6c062e2d60c
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-16 18:26:29 +0500

    Update Home page route

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Footer from "./fragments/Footer";
 import Main from "./fragments/Main";
-import Login from "./fragments/login/Login";
 import Cars from "./fragments/navigation/Cars";
+import Body from "./fragments/Initialpage/initial";
 
 export default () => (
   <>
-    <Login/>
     {/* Navbar goes here */}
     <Cars />
 
