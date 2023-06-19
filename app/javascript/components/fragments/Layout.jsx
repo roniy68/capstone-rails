c20230c8ba0a39f8722db5e6696d0e4e96169b5b
roniy68 <ahmed.11900449@lpu.in>
2023-06-19 15:10:05 +0530

    🔥 Main Container widh-full

--- app/javascript/components/fragments/Layout.jsx
+++ app/javascript/components/fragments/Layout.jsx
@@ -8,11 +8,11 @@ const Layout = ({ children }) => {
                 <div className="block w-1/3 lg:w-1/5 bg-red-600 text-[#444444] h-screen mb-[200px]">
                     <Sidebar />
                 </div>
-                <div className="flex bg-gray-500">
+                <div className="flex bg-gray-500 w-full h-full">
 
                     <div className="flex flex-col items-center">
                         <div className="bg-blue-500 h-auto">
-                            Navbar top
+                            Navbar top eited
                         </div>
                         <div className=''>
                             {/* Children Goes Here */}
