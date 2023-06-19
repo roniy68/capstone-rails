6acb669c8657b49ec71ba35906ed37770001b6bd
Salwa99 <s.ritage99@gmail.com>
2023-06-19 14:55:01 +0100

    Adjust color in the sidebar

--- app/javascript/components/fragments/Sidebar.jsx
+++ app/javascript/components/fragments/Sidebar.jsx
@@ -4,27 +4,27 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
     return (
-        <>
-            <div className="mb-20 p-10 bg-red-300 rounded-full">
-                <img src={logo} alt="logo" height="10px" />
-            </div>
+      <>
+        <div className="mb-20 p-10 bg-red-300 rounded-full">
+          <img src={logo} alt="logo" height="10px" />
+        </div>
 
-            <ul className="py-4">
-                <Link to="/cars">
-                    <li className="px-4 py-2 hover:bg-green-700">MODELS</li>
-                </Link>
-                <Link to="/reserve">
-                    <li className="px-4 py-2 hover:bg-green-700">ADD RESERVATION</li>
-                </Link>
-                <Link to="/addcar">
-                    <li className="px-4 py-2 hover:bg-green-700">ADD CAR</li>
-                </Link>
-                <Link to="/myreservations">
-                    <li className="px-4 py-2 hover:bg-green-700">MY RESERVATIONS</li>
-                </Link>
-            </ul>
-        </>
-    )
+        <ul className="py-4 colorHover">
+          <Link to="/cars">
+            <li className="px-4 py-2">MODELS</li>
+          </Link>
+          <Link to="/reserve">
+            <li className="px-4 py-2">ADD RESERVATION</li>
+          </Link>
+          <Link to="/addcar">
+            <li className="px-4 py-2">ADD CAR</li>
+          </Link>
+          <Link to="/myreservations">
+            <li className="px-4 py-2">MY RESERVATIONS</li>
+          </Link>
+        </ul>
+      </>
+    );
 }
 
 export default Sidebar
--- app/javascript/components/pages/CarDetailPage.jsx
+++ app/javascript/components/pages/CarDetailPage.jsx
@@ -24,7 +24,7 @@ const CarDetailPage = () => {
   if (!car) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <p className="text-gray-500 text-lg">Loading...</p>
+        <p className="text-green-500 text-lg">Loading...</p>
       </div>
     );
   }
