c0a7c6d1d575345ce61da70bb451808b308a2f44
roniy68 <ahmed.11900449@lpu.in>
2023-06-20 17:03:33 +0530

    🔥 Configure Shrine : Need Testing

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -8,13 +8,12 @@ if  Rails.env.test?
   }
 else
  Shrine.storages = {
-   cache: Shrine::Storage::FileSystem.new("public", prefix:     "uploads/cache"), # temporary
+   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
  }
 end
 Shrine.plugin :activerecord    # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data  # extracts metadata for assigned cached files
-Shrine.plugin :validation_helpers
-Shrine.plugin :validation
-‍
+# Shrine.plugin :validation_helpers
+# Shrine.plugin :validation
