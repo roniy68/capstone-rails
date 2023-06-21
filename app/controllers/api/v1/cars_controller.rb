10b57693a1b83991268327e4ff1280258963f97e
Salwa99 <s.ritage99@gmail.com>
2023-06-21 23:39:34 +0100

    Update the car controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,45 +1,23 @@
 class Api::V1::CarsController < Api::V1::BaseController
-  # include ActiveStorage::SetCurrent
-  # include Rails.application.routes.url_helpers
-
   before_action :set_car, only: %i[show destroy]
 
   def index
     cars = Car.all.order(created_at: :desc)
-
-    # image_url = rails_blob_path(cars.image, disposition: "attachment", only_path: true)
-    
-    # This works to add image blob but not url
-    render json: cars.as_json(include: :image)
-    
-    # doesnot work
-    # render json: cars.map{ |car|
-    #  car.as_json.merge({ image: url_for(car.image)})
-    # }
-
-    # render json: cars.as_json(include: :image).map do |car|
-    #   ActiveStorage::Current.set(host: "http://localhost:3000") do
-    #     car.merge(image: car.image.url)
-    #   end
-    # end
-
-    # render json: cars.as_json(include: :image).map do |car|
-    #   car.as_json.merge(image: "hello")
-    # end
+    render json: cars
   end
 
   def create
-    car = Car.create!(car_params)
-    car.user = current_user
-    if car
-      render json: car
+    user = User.find(params[:user_id])
+    car = user.cars.build(car_params)
+    if car.save
+      render json: car, status: :created
     else
-      render json: car.errors
+      render json: { error: car.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def show
-    render json: @car.as_json(include: :image).merge(url_for(image))
+    render json: @car
   end
 
   def destroy
@@ -50,7 +28,7 @@ class Api::V1::CarsController < Api::V1::BaseController
   private
 
   def car_params
-    params.permit(:name, :model, :price, :description, :photo, :user_id,  :image)
+    params.require(:car).permit(:name, :model, :price, :description, :photo, :user_id)
   end
 
   def set_car
