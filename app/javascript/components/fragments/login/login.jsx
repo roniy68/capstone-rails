3d22a9bad6e21c38d523efd5532a057feecf1040
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-21 16:22:42 +0500

    Update login css

--- app/javascript/components/fragments/login/login.jsx
+++ app/javascript/components/fragments/login/login.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
-import '../../../../assets/stylesheets/navigationStyle/log.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
