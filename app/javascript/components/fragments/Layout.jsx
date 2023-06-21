cef6d5e1fd07a7d94361b94bec2ac197f1fdca05
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-06-21 15:03:03 +0500

    Update logout function

--- app/javascript/components/fragments/Layout.jsx
+++ app/javascript/components/fragments/Layout.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import Sidebar from './Sidebar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineLogout } from "react-icons/ai";
 
 const Layout = () => {
+  const Navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("username");
+    Navigate("/login", { state: { message: "Signed out successfully" } });
+  };
     return (
       <>
           <div className="main-container">
             <div className="navbar-top">
               <div className="logo-ham">
-                <GiHamburgerMenu className="menu-icons" />
+                <GiHamburgerMenu className="menu-icons"  />
                 <div className="logo">
                   <p>RENTCAR</p>
                 </div>
               </div>
-              <AiOutlineLogout className="menu-icons" />
+              <AiOutlineLogout className="menu-icons" onClick={handleLogout} />
             </div>
 
             <Sidebar />
