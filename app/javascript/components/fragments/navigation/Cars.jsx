84d0705f86a4e2da931a9c54bd7855d8c378701d
Salwa99 <s.ritage99@gmail.com>
2023-06-22 21:31:30 +0100

    Fix the carousel responsiveness

--- app/javascript/components/fragments/navigation/Cars.jsx
+++ app/javascript/components/fragments/navigation/Cars.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCars, selectCars } from "../../redux/carsSlice";
 import { Link, useNavigate } from "react-router-dom";
@@ -14,12 +14,16 @@ import { useLocation } from "react-router-dom";
 const Cars = () => {
   const dispatch = useDispatch();
   const { cars, isLoading } = useSelector(selectCars);
-  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const location = useLocation();
-  const [message, setMessage] = React.useState(location?.state?.message || "");
+  const [message, setMessage] = useState(location?.state?.message || "");
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     dispatch(fetchCars());
+    checkMobileView();
+    window.addEventListener("resize", checkMobileView);
+    return () => window.removeEventListener("resize", checkMobileView);
   }, [dispatch]);
 
   useEffect(() => {
@@ -32,6 +36,14 @@ const Cars = () => {
     }
   }, [message]);
 
+  const checkMobileView = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => prevIndex - 1);
   };
@@ -42,14 +54,21 @@ const Cars = () => {
 
   const renderCars = () => {
     const startIndex = currentIndex;
-    const endIndex = startIndex + 2;
+    const endIndex = startIndex + (isMobile ? 0 : 2);
     return cars.slice(startIndex, endIndex + 1).map((car) => (
-      <li key={car.id} className="w-auto h-auto lg:w-[400px] lg:h-[400px] mt-10 shadow">
+      <li
+        key={car.id}
+        className={`w-auto h-auto mt-10 shadow ${
+          isMobile ? "w-[100%]" : "lg:w-[400px] lg:h-[400px]"
+        }`}
+      >
         <Link to={`/detail/${car.id}`}>
           <img
             src={car.photo}
             alt={car.name}
-            className="w-[200px] h-[200px] mx-auto my-4"
+            className={`w-[200px] h-[200px] mx-auto my-4 ${
+              isMobile ? "w-[100%]" : ""
+            }`}
           />
           <div className="text-center">
             <p>
@@ -77,9 +96,7 @@ const Cars = () => {
   }
 
   return (
-
-    <div className="bg-green-900 h-auto w-auto">
-
+    <div className="h-auto w-auto">
       {message && (
         <p className="text-center text-green-600 text-2xl">{message}</p>
       )}
@@ -94,23 +111,25 @@ const Cars = () => {
         <button
           onClick={handlePrev}
           disabled={currentIndex === 0}
-          className="bg-[#96bf01] rounded-r-full mr-6 pl-10"
+          className="bg-[#96bf01] rounded-r-full mr-2 pl-4 lg:mr-6 lg:pl-10"
         >
           <BsCaretLeft size={50} className="text-white" />
         </button>
-        <ul className=" lg:w-[800px] gap-4 flex-1 flex justify-center items-center">
+        <ul
+          className={`lg:w-[800px] gap-4 flex-1 flex justify-center items-center ${
+            isMobile ? "flex-wrap" : ""
+          }`}
+        >
           {renderCars()}
         </ul>
         <button
           onClick={handleNext}
-          disabled={currentIndex >= cars.length - 3}
-          className="bg-[#96bf01] rounded-l-full ml-6 pr-10"
+          disabled={currentIndex >= cars.length - (isMobile ? 1 : 3)}
+          className="bg-[#96bf01] rounded-l-full ml-2 pr-4 lg:ml-6 lg:pr-10"
         >
           <BsCaretRight size={50} className="text-white" />
         </button>
       </div>
-
-
     </div>
   );
 };
