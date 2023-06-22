bfce72a9e0583b6b2a52ceef16cc3adf8f33a320
roniy68 <ahmed.11900449@lpu.in>
2023-06-22 20:35:01 +0530

    🔥 Staging Deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,3 +7,4 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
 bundle exec rails db:migrate
+bundle exec rails db:seed
