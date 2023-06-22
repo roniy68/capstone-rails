497216c8ded48618b5a744c5a8f925fd07b4e3e6
roniy68 <ahmed.11900449@lpu.in>
2023-06-23 02:30:21 +0530

    🔥 Deploy : without Data Change

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,5 +7,5 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 # bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
